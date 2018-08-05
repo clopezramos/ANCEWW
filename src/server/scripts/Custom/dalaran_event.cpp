ee514aae4545d7193118dab5a70a33ce22ceb777
clopezramos <clopezdist@gmail.com>
2018-08-05 23:51:32 +0200

    placeholders

--- src/server/scripts/Custom/dalaran_event.cpp
+++ src/server/scripts/Custom/dalaran_event.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008-2018 TrinityCore <https://www.trinitycore.org/>
+ * Copyright (C) 2018 clopezramos <https://github.com/clopezramos>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -15,12 +16,8 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// This is where scripts' loading functions should be declared:
-void AddDalaranEventScripts();
+#include "ScriptMgr.h"
 
-// The name of this function should match:
-// void Add${NameOfDirectory}Scripts()
-void AddCustomScripts()
+void AddDalaranEventScripts()
 {
-    AddDalaranEventScripts();
 }
