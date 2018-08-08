f5e6f4f4895ae0fa28e30bd1808b07a7f0b2e5bb
clopezramos <clopezdist@gmail.com>
2018-08-08 02:47:58 +0200

    Misc: Original Rhonin quest event

---
+++ sql/custom/world/custom_2018_08_07_00_world.sql
@@ -0,0 +1,88 @@
+--
+-- Rhonin (original)
+--
+
+-- New gossip condition - must have quest 30000
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=10140 AND `SourceEntry`=0;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(15, 10140, 0, 0, 0, 9, 0, 30000, 0, 0, 0, "", "Manabonk Escape - Rhonin (original) - gossip_menu_option OptionID 9 - quest 30000");
+
+-- Assign quest
+DELETE FROM `creature_queststarter` WHERE (quest = 30000) AND (id IN (16128));
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
+(16128, 30000);
+
+-- Updated SAI
+
+-- Original SAI (for reference)
+-- DELETE FROM `smart_scripts` WHERE (`entryorguid`=16128 AND `source_type`=0);
+-- INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+-- (16128, 0, 0, 5, 20, 0, 100, 512, 13631,     0, 0, 0, 0, 80, 1612800, 2, 0, 0, 0, 0,  1,     0, 0, 0, 0, 0, 0, 0, "Rhonin - On Quest Reward (All Is Well That Ends Well) - Run Script"),
+-- (16128, 0, 1, 5, 20, 0, 100, 512, 13819,     0, 0, 0, 0, 80, 1612800, 2, 0, 0, 0, 0,  1,     0, 0, 0, 0, 0, 0, 0, "Rhonin - On Quest Reward (Heroic: All Is Well That Ends Well) - Run Script"),
+-- (16128, 0, 2, 0, 38, 0, 100, 512,     1,     1, 0, 0, 0, 80, 1612801, 2, 0, 0, 0, 0,  1,     0, 0, 0, 0, 0, 0, 0, "Rhonin - On Data Set 1 1 - Run Script 2"),
+-- (16128, 0, 3, 0, 40, 0, 100, 512,    11, 16128, 0, 0, 0, 80, 1612802, 2, 0, 0, 0, 0,  1,     0, 0, 0, 0, 0, 0, 0, "Rhonin - On Reached WP11 - Run Script 3"),
+-- (16128, 0, 4, 5, 38, 0, 100, 512,     6,     6, 0, 0, 0, 80, 1612800, 2, 0, 0, 0, 0,  1,     0, 0, 0, 0, 0, 0, 0, "Rhonin - On Data Set 6 6 - Run Script"),
+-- (16128, 0, 5, 0, 61, 0, 100, 512,     0,     0, 0, 0, 0, 45,       1, 1, 0, 0, 0, 0, 19, 30116, 0, 0, 0, 0, 0, 0, "Rhonin - Link - Set Data to Archmage Aethas Sunreaver <Kirin Tor>");
+
+-- DELETE FROM `smart_scripts` WHERE (`entryorguid`=1612800 AND `source_type`=9);
+-- INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+-- (1612800, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 81,     0, 0,      0, 0, 0, 0, 1, 0, 0, 0,      0,       0,       0,       0, "Rhonin - Script - Set NPC Flags"),
+-- (1612800, 9, 1, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 12, 34044, 1, 300000, 0, 0, 0, 8, 0, 0, 0, 5800.3, 822.453, 668.519, 4.81281, "Rhonin - Script - Summon Brann Bronzebeard"),
+-- (1612800, 9, 2, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0,  1,     0, 0,      0, 0, 0, 0, 8, 0, 0, 0,      0,       0,       0,       0, "Rhonin - Script - Say Line 1");
+
+-- DELETE FROM `smart_scripts` WHERE (`id`=1612801);
+-- INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+-- (1612801, 9, 0, 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 0, 53, 0, 16128, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 2 - Start WP');
+
+SET @ENTRY := 16128;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY, 0, 0,   5, 20, 0, 100, 512, 13631,     0, 0, 0, 80, 1612800, 2, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Quest 'All Is Well That Ends Well' Finished - Run Script"),
+(@ENTRY, 0, 1,   5, 20, 0, 100, 512, 13819,     0, 0, 0, 80, 1612800, 2, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Quest 'Heroic: All Is Well That Ends Well' Finished - Run Script"),
+(@ENTRY, 0, 2,   0, 38, 0, 100, 512,     1,     1, 0, 0, 80, 1612801, 2, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Data Set 1 1 - Run Script"),
+(@ENTRY, 0, 3,   0, 40, 0, 100, 512,    11, 16128, 0, 0, 80, 1612802, 2, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Waypoint 11 Reached - Run Script"),
+(@ENTRY, 0, 4,   5, 38, 0, 100, 512,     6,     6, 0, 0, 80, 1612800, 2, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Data Set 6 6 - Run Script"),
+(@ENTRY, 0, 5,   0, 61, 0, 100, 512,     0,     0, 0, 0, 45,       1, 1, 0, 0, 0, 0, 19, 30116,  0, 0, 0, 0, 0, 0, "Rhonin - On Link - Set Data 1 1"),
+(@ENTRY, 0, 6,  12, 62, 0, 100, 512, 10140,     0, 0, 0, 64,       1, 0, 0, 0, 0, 0,  7,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Gossip Option 0 Selected - Store Targetlist"),
+(@ENTRY, 0, 7,  13, 38, 0, 100, 512,     9,     1, 0, 0, 11,   62940, 0, 0, 0, 0, 0, 12,     1,  0, 0, 0, 0, 0, 0, "Rhonin - On Data Set 9 1 - Cast 'Teleport'"),
+(@ENTRY, 0, 8,   9, 61, 0, 100, 512,     0,     0, 0, 0, 72,       0, 0, 0, 0, 0, 0, 17,     0, 20, 0, 0, 0, 0, 0, "Rhonin - On Link - Close Gossip"),
+(@ENTRY, 0, 9,   0, 61, 0, 100, 512,     0,     0, 0, 0, 80, 1612809, 2, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Link - Run Script"),
+(@ENTRY, 0, 10,  0, 62, 0, 100, 512, 10140,     0, 0, 0, 80, 1612810, 2, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Gossip Option 0 Selected - Run Script"),
+(@ENTRY, 0, 11,  0, 62, 0, 100, 512, 10140,     0, 0, 0, 80, 1612811, 2, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Gossip Option 0 Selected - Run Script"),
+(@ENTRY, 0, 12,  8, 61, 0, 100, 512,     0,     0, 0, 0, 83,       1, 0, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Link - Remove Npc Flag Gossip"),
+(@ENTRY, 0, 13,  0, 61, 0, 100, 512,     0,     0, 0, 0, 82,       1, 0, 0, 0, 0, 0,  1,     0,  0, 0, 0, 0, 0, 0, "Rhonin - On Link - Add Npc Flag Gossip");
+
+SET @ENTRY := 1612809;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=9;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY, 9, 0, 0, 0, 0, 100, 0,    0,    0, 0, 0,  1, 8, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "On Script - Say Line 8"),
+(@ENTRY, 9, 1, 0, 0, 0, 100, 0, 3000, 3000, 0, 0,  1, 9, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "On Script - Say Line 9"),
+(@ENTRY, 9, 2, 0, 0, 0, 100, 0, 5000, 5000, 0, 0, 45, 9, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "On Script - Set Data 9 1");
+
+SET @ENTRY := 1612810;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=9;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY, 9, 0, 0, 0, 0, 100, 0,    0,    0, 0, 0, 1, 10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Rhonin - On Script - Say Line 10"),
+(@ENTRY, 9, 1, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 1, 11, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Rhonin - On Script - Say Line 11");
+
+SET @ENTRY := 1612811;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=9;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 1, 12, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Rhonin - On Script - Say Line 12");
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=22 AND `SourceGroup`=7 AND `SourceEntry`=16128;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=22 AND `SourceGroup`=12 AND `SourceEntry`=16128;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=22 AND `SourceGroup`=11 AND `SourceEntry`=16128;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(22,  7, 16128, 0, 0, 32, 0, 0x0010, 0, 0, 0, "condition_minigob_event_in_group", ""),
+(22, 12, 16128, 0, 0, 32, 0, 0x0010, 0, 0, 0,  "condition_minigob_event_in_raid", ""),
+(22, 11, 16128, 0, 0, 32, 0, 0x0010, 0, 0, 0,    "condition_minigob_event_alone", "");
+
+-- Random teleport spell
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (62940);
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(62940, 47653, 0, 'Minigob Escape - Teleport Random');
+
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (47653);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(47653, 'spell_minigob_escape_teleport_random');
--- src/server/scripts/Custom/dalaran_event.cpp
+++ src/server/scripts/Custom/dalaran_event.cpp
@@ -16,8 +16,168 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "ConditionMgr.h"
+#include "Common.h"
+#include "Group.h"
+#include "GroupReference.h"
+#include "Object.h"
+#include "Player.h"
+#include "Position.h"
+#include "QuestDef.h"
+#include "Random.h"
 #include "ScriptMgr.h"
+#include "SpellInfo.h"
+#include "SpellScript.h"
+#include "Unit.h"
+
+enum MinigobEscapeMisc
+{
+    QUEST_MINIGOB_ESCAPE = 30000,
+
+    NPC_RHONIN_ORIGINAL = 16128,
+};
+
+WorldLocation static const randomTeleport[] =
+{
+    { 1u,   Position(16220.393f, 16403.613f, -64.37996f,  6.23f) },
+    { 571u, Position(5664.837f,  2013.375f,  1544.07031f, 3.58f) },
+    { 1u,   Position(-8827.265f, -4758.390f, 1.91664f,    4.57f) }
+};
+
+// 47653 - Teleport Self
+class spell_minigob_escape_teleport_random : public SpellScript
+{
+    PrepareSpellScript(spell_minigob_escape_teleport_random);
+
+    bool Load() override
+    {
+        if (GetCaster()->GetEntry() == NPC_RHONIN_ORIGINAL)
+            return true;
+
+        return false;
+    }
+
+    void Teleport()
+    {
+        PreventHitEffect(EFFECT_0);
+
+        Player* target = GetHitPlayer();
+        if (!target)
+            return;
+
+        if (target->IsGameMaster())
+        {
+            target->TeleportTo(randomTeleport[urand(0, 2)]);
+            //target->SetPhaseMask(2, true);
+            return;
+        }
+
+        Group* group = target->GetGroup();
+        if (!group)
+            return;
+
+        bool valid = false;
+        for (GroupReference* itr = group->GetFirstMember(); itr != nullptr && !valid; itr = itr->next())
+        {
+            if (Player* member = itr->GetSource())
+                if (member->GetQuestStatus(QUEST_MINIGOB_ESCAPE) == QUEST_STATUS_INCOMPLETE)
+                    valid = true;
+        }
+
+        if (!valid)
+            return;
+
+        for (GroupReference* itr = group->GetFirstMember(); itr != nullptr; itr = itr->next())
+        {
+            if (Player* member = itr->GetSource())
+            {
+                if (member->IsAlive() && member->IsWithinDistInMap(GetCaster(), GetSpellInfo()->GetMaxRange()))
+                {
+                    member->TeleportTo(randomTeleport[urand(0, 2)]);
+                    //member->SetPhaseMask(2, true);
+                }
+            }
+        }
+    }
+
+    void Register() override
+    {
+        OnHit += SpellHitFn(spell_minigob_escape_teleport_random::Teleport);
+    }
+};
+
+class condition_minigob_event_in_group : public ConditionScript
+{
+    public:
+        condition_minigob_event_in_group() : ConditionScript("condition_minigob_event_in_group") { }
+
+        bool OnConditionCheck(Condition const* condition, ConditionSourceInfo& sourceInfo) override
+        {
+            WorldObject* target = sourceInfo.mConditionTargets[condition->ConditionTarget];
+            if (Player* player = target->ToPlayer())
+            {
+                if (Group* group = player->GetGroup())
+                    if (!group->isRaidGroup() && group->GetMembersCount() >= 3)
+                        return true;
+
+                if (player->IsGameMaster())
+                    return true;
+            }
+
+            return false;
+        }
+};
+
+class condition_minigob_event_in_raid : public ConditionScript
+{
+    public:
+        condition_minigob_event_in_raid() : ConditionScript("condition_minigob_event_in_raid") { }
+
+        bool OnConditionCheck(Condition const* condition, ConditionSourceInfo& sourceInfo) override
+        {
+            WorldObject* target = sourceInfo.mConditionTargets[condition->ConditionTarget];
+            if (Player* player = target->ToPlayer())
+            {
+                if (Group* group = player->GetGroup())
+                    if (group->isRaidGroup())
+                        return true;
+            }
+
+            return false;
+        }
+};
+
+class condition_minigob_event_alone : public ConditionScript
+{
+    public:
+        condition_minigob_event_alone() : ConditionScript("condition_minigob_event_alone") { }
+
+        bool OnConditionCheck(Condition const* condition, ConditionSourceInfo& sourceInfo) override
+        {
+            WorldObject* target = sourceInfo.mConditionTargets[condition->ConditionTarget];
+            if (Player* player = target->ToPlayer())
+            {
+                if (player->IsGameMaster())
+                    return false;
+
+                if (Group* group = player->GetGroup())
+                {
+                    if (group->GetMembersCount() < 3)
+                        return true;
+                }
+                else
+                    return true;
+            }
+
+            return false;
+        }
+};
 
 void AddDalaranEventScripts()
 {
+    RegisterSpellScript(spell_minigob_escape_teleport_random);
+
+    new condition_minigob_event_alone();
+    new condition_minigob_event_in_group();
+    new condition_minigob_event_in_raid();
 }
