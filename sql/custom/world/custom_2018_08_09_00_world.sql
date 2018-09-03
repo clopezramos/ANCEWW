2e3fa015580227fe4e16a53da8545d4902ef5ef3
clopezramos <clopezdist@gmail.com>
2018-09-03 20:02:00 +0200

    AI: Minigob clone SAI

--- sql/custom/world/custom_2018_08_09_00_world.sql
+++ sql/custom/world/custom_2018_08_09_00_world.sql
@@ -113,3 +113,9 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY+20, 0, 1, 0,  4, 0, 100, 0,     0,     0,     0,     0, 11, 47075, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Minigob Escape - Elemental de Fuego esclavizado - On Aggro - Cast 'Immolation'"),
 (@ENTRY+20, 0, 2, 0,  0, 0, 100, 0, 10000, 10000, 10000, 10000, 11, 60744, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Minigob Escape - Elemental de Fuego esclavizado - In Combat - Cast 'Immolation'"),
 (@ENTRY+20, 0, 3, 0, 23, 0, 100, 1, 70380,     1,     0,     0, 37,     0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Minigob Escape - Elemental de Fuego esclavizado - On Has Aura 'Deep Freeze' - Kill Self");
+
+-- Minigob Manacapón SAI
+UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY+01;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY+01 AND `source_type`=0;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY+01, 0, 0, 0, 0, 0, 100, 0, 1000, 4000, 2000, 5000, 11, 61834, 96, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, "Minigob Escape - Minigob Manacapón - In Combat - Cast 'Manabonked!'");
